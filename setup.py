9784023cc3517a1a0b67fae47f43ba3758aabe0f
Rehan Guha <rehanguha29@gmail.com>
2019-11-28 16:26:19 +0530

    Cosine Similarity

--- setup.py
+++ setup.py
@@ -5,18 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="interspace", # Replace with your own username
-    version="0.0.2",
+    version="0.0.5",
     author="Rehan Guha",
     py_modules=["interspace"],
     author_email="rehanguha29@gmail.com",
-    description="A package to claculate various distances given 2 points as input",
+    description="Interspace gives us different type distances between two vectors.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rehanguha/interspace",
-    #package_dir={'': 'src'},
+    package_dir={'': 'src'},
     packages=setuptools.find_packages(),
     classifiers=[
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering"
     ],
     python_requires='>=2.7',
+    install_requires=['numpy'],
 )
--- src/interspace.py
+++ src/interspace.py
@@ -1,18 +1,30 @@
+import numpy as np
+
 # calculate the Euclidean distance between two vectors
 def euclidean(vector_1, vector_2):
     distance = 0.0
-    if len(vector_1) == len(vector_2):
-    	for i in range(len(vector_1)):
-    		distance += (vector_1[i] - vector_2[i])**2
-    	return (distance)**0.5
-    else:
-        raise Exception("Vector lengths mismatch")
+    for i in range(len(vector_1)):
+            distance += (vector_1[i] - vector_2[i])**2 
+    return (distance)**0.5
 
+def minkowski(vector_1, vector_2, p=1):
+    distance = 0.0
+    for i in range(len(vector_1)):
+            distance += (vector_1[i] - vector_2[i])**p
+    return (distance)**(1.0/p)
 
-def distance(vector_1, vector_2):
+def cosine_similarity(vector_1, vector_2):
+    return np.dot(vector_1, vector_2) / ((np.sqrt(np.dot(vector_1, vector_1)) * np.sqrt(np.dot(vector_2, vector_2))))
+
+def distance(vector_1, vector_2, p=1):
+    if len(vector_1) != len(vector_2):
+        return "Vector lengths mismatch"
+    
     distances = {}
-    try:
-        distances['Euclidean'] = euclidean(vector_1, vector_2)
-        return distances
-    except:
-        print("Vector lengths mismatch")
+    
+    distances['Euclidean'] = euclidean(vector_1, vector_2)
+    distances[str('Minkowski-')+str(p)] = minkowski(vector_1, vector_2, p=p)
+    distances['Cosine Similarity'] = cosine_similarity(vector_1, vector_2)
+
+        
+    return distances
