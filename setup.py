cdd81728814c462364aea5c4b2f0ff4180b167c8
rehanguha <>
2021-08-30 18:07:55 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="interspace", # Replace with your own username
-    version="0.0.9",
+    version="0.0.10",
     author="Rehan Guha",
     py_modules=["interspace"],
     license='mit',
